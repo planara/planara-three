a91a995a9188fe9173b45671d417b3aa865e405d
a1unade <tags0956@gmail.com>
2025-11-16 20:16:04 +0300

    [fix] package dependencies

--- src/orbit/orbit-extension.ts
+++ src/orbit/orbit-extension.ts
@@ -1,5 +1,5 @@
 // Core
-import { Camera } from 'three';
+import { type Camera } from 'three';
 // Extensions
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
@@ -7,29 +7,32 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
  * Расширение для OrbitControls с отслеживанием управления камерой
  * @public
  */
-export class OrbitWithState extends OrbitControls implements Disposable {
+export class OrbitWithState extends OrbitControls {
   /** Используется ли Orbit-controls */
   public isInteracting: boolean = false;
 
-  constructor(camera: Camera, domElement?: HTMLElement | null) {
+  private onMouseDown = () => (this.isInteracting = true);
+  private onMouseUp = () => (this.isInteracting = false);
+  private onTouchStart = () => (this.isInteracting = true);
+  private onTouchEnd = () => (this.isInteracting = false);
+
+  public constructor(camera: Camera, domElement?: HTMLElement | null) {
     super(camera, domElement);
 
     // Добавление обработчиков событий
-    this.domElement?.addEventListener('mousedown', () => (this.isInteracting = true));
-    this.domElement?.addEventListener('mouseup', () => (this.isInteracting = false));
-    this.domElement?.addEventListener('touchstart', () => (this.isInteracting = true));
-    this.domElement?.addEventListener('touchend', () => (this.isInteracting = false));
+    this.domElement?.addEventListener('mousedown', this.onMouseDown);
+    this.domElement?.addEventListener('mouseup', this.onMouseUp);
+    this.domElement?.addEventListener('touchstart', this.onTouchStart);
+    this.domElement?.addEventListener('touchend', this.onTouchEnd);
   }
 
   /** Очистка новых обработчиков событий */
-  public dispose(): void {
-    this.domElement?.removeEventListener('mousedown', () => (this.isInteracting = true));
-    this.domElement?.removeEventListener('mouseup', () => (this.isInteracting = false));
-    this.domElement?.removeEventListener('touchstart', () => (this.isInteracting = true));
-    this.domElement?.removeEventListener('touchend', () => (this.isInteracting = false));
-  }
+  public dispose() {
+    this.domElement?.removeEventListener('mousedown', this.onMouseDown);
+    this.domElement?.removeEventListener('mouseup', this.onMouseUp);
+    this.domElement?.removeEventListener('touchstart', this.onTouchStart);
+    this.domElement?.removeEventListener('touchend', this.onTouchEnd);
 
-  [Symbol.dispose](): void {
-    this.dispose();
+    super.dispose();
   }
 }
