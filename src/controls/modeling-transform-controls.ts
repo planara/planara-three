0011d558c0c0519564176749382be999812f287b
a1unade <tags0956@gmail.com>
2025-11-21 23:00:27 +0300

    [add] vertex modeling case, select-handler and transform controls

--- src/controls/modeling-transform-controls.ts
+++ src/controls/modeling-transform-controls.ts
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import { TransformControls } from 'three/addons/controls/TransformControls.js';
 // Types
 import type { EdgeInfo } from '../types/controls/edge-info';
+import type { VertexInfo } from '../types/controls/vertex-info';
 
 /** Расширенные `TransformControls` для редактирования точек/ребер/граней модели */
 export class ModelingTransformControls extends TransformControls {
@@ -16,11 +17,25 @@ export class ModelingTransformControls extends TransformControls {
     const obj = this.object as THREE.Object3D | null;
     if (!obj) return;
 
+    // вершина
+    const vertex: VertexInfo | undefined = (obj as any).userData?.vertexInfo;
+    if (vertex) {
+      if (this.mode === 'translate') {
+        this._applyVertexTranslate(obj as THREE.Points, vertex);
+      }
+      return;
+    }
+
+    // ребро
     const edge: EdgeInfo | undefined = (obj as any).userData?.edgeInfo;
-    if (!edge) return;
+    if (edge) {
+      if (this.mode === 'translate') {
+        this._applyEdgeTranslate(obj as THREE.Line, edge);
+      }
+      return;
+    }
 
     // TODO: добавить сохранение предыдущего состояния, чтобы можно было откатиться назад
-    if (this.mode === 'translate' && edge) this._applyEdgeTranslate(obj as THREE.Line, edge);
   }
 
   private _applyEdgeTranslate(line: THREE.Line, edge: EdgeInfo) {
@@ -79,5 +94,51 @@ export class ModelingTransformControls extends TransformControls {
       lines.geometry.computeBoundingBox();
       lines.geometry.computeBoundingSphere();
     }
+  }
+
+  private _applyVertexTranslate(proxy: THREE.Points, v: VertexInfo) {
+    const { mesh, vertexIndices, lines, edgeVertexIndices } = v;
+    if (!mesh || !vertexIndices?.length) return;
+
+    // Получение позиции прокси-вершины
+    const P_world = proxy.position.clone();
+
+    // Применение трансформаций на фигуру
+    const toLocalMesh = new THREE.Matrix4().copy(mesh.matrixWorld).invert();
+    const P_localMesh = P_world.clone().applyMatrix4(toLocalMesh);
+
+    const meshGeo = mesh.geometry as THREE.BufferGeometry;
+    const meshPos = meshGeo.getAttribute('position') as THREE.BufferAttribute;
+
+    for (const idx of vertexIndices) {
+      meshPos.setXYZ(idx, P_localMesh.x, P_localMesh.y, P_localMesh.z);
+    }
+    meshPos.needsUpdate = true;
+
+    meshGeo.computeVertexNormals();
+    meshGeo.computeBoundingBox();
+    meshGeo.computeBoundingSphere();
+
+    // Применение трансформаций на внешние ребра фигуры
+    if (lines && edgeVertexIndices?.length) {
+      const toLocalLines = new THREE.Matrix4().copy(lines.matrixWorld).invert();
+      const P_localLines = P_world.clone().applyMatrix4(toLocalLines);
+
+      const lPos = lines.geometry.getAttribute('position') as THREE.BufferAttribute;
+      for (const idx of edgeVertexIndices) {
+        lPos.setXYZ(idx, P_localLines.x, P_localLines.y, P_localLines.z);
+      }
+      lPos.needsUpdate = true;
+
+      lines.geometry.computeBoundingBox();
+      lines.geometry.computeBoundingSphere();
+    }
+
+    // Обновление позиции прокси-вершины
+    const pPos = (proxy.geometry as THREE.BufferGeometry).getAttribute(
+      'position',
+    ) as THREE.BufferAttribute;
+    pPos.setXYZ(0, 0, 0, 0);
+    pPos.needsUpdate = true;
   }
 }
